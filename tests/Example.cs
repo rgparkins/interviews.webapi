f334ec7e266a2c34019ede74edb53a5d869ef6e2
Richard Parkins <richard@saucar.co.uk>
2017-04-10 09:34:57 +0100

    udpate

---
+++ tests/Example.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Parkwell.cms.tests
+{
+    public class BlankTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
